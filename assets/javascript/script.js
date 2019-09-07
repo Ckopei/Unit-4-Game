793aa4e5a93b20716cacae483f4a191cc9911cd5
Ckopei <cameron@kopels.com>
2019-09-07 00:58:37 -0400

    Wireframe/pseudocode written for javascript. Readme completed.

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+//theme of game TBD. Use base for now, change styling later. styling does not matter until script is completed.
+
+//player is shown a random goal number to work to. 19-120. 
+//Math.floor((Math.random() * 102) + 19);
+//math.floor with a range needs the range between the two numbers(+1) + the lower number. -TESTED ON W3SCHOOLS
+
+//4 total buttons, bottom of page. 
+//each button (crystal) needs to have a random number. (math.floor(math.random)) range of 1-12
+//Math.floor((Math.random() * 12) + 1);
+//math.floor with a range needs the range between the two numbers(+1) + the lower number.
+
+//number needs to be hidden besides from watching it add to player total. Display player total.
+
+//add random button number to total score
+
+//player wins if total score matches the random goal number. wins ++
+
+//player busts if it goes over. losses ++
+
+//game restarts regardless of outcome
+
+//reset() for all numbers, no page refresh.
+
+//display wins+losses directly on page with $("#id").text("string"). 
+
+//Probably use <P> <spans> </P>. If not spans, use $("#id").html(wins)
