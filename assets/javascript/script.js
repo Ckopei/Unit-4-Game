f35681afe0717c6c3ce4c6ebcda3579efd1a2433
Ckopei <cameron@kopels.com>
2019-09-08 19:34:45 -0400

    fixed issues with wins and losses displaying, changed if/else if logic for a switch statement. game currently works.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,11 +6,7 @@
 var computerNum = Math.floor((Math.random() * 102) + 19);
 $(".computerNum").html(computerNum)
 var wins = 0;
-//write wins to html's span for wins
-$("#wins").html(wins)
 var losses = 0;
-//write losses to html's span for losses.
-$("#losses").html(losses)
 //initial total to work from. going forward use value attr and pull from it for math
 var playerTotal = 0;
 //4 total buttons, bottom of page. 
@@ -19,55 +15,81 @@ var playerTotal = 0;
 //Math.floor((Math.random() * 12) + 1);
 //math.floor with a range needs the range between the two numbers(+1) + the lower number.
 var button1 = Math.floor((Math.random() * 12) + 1);
-$(".button1").attr("value",button1)
+$(".button1").attr("value", button1)
 
 var button2 = Math.floor((Math.random() * 12) + 1);
-$(".button2").attr("value",button2)
+$(".button2").attr("value", button2)
 
 var button3 = Math.floor((Math.random() * 12) + 1);
-$(".button3").attr("value",button3)
+$(".button3").attr("value", button3)
 
 var button4 = Math.floor((Math.random() * 12) + 1);
-$(".button4").attr("value",button4)
+$(".button4").attr("value", button4)
 console.log($(".button4").attr)
 
-
-//number needs to be hidden besides from watching it add to player total. Display player total.
-
-//reset function
-var reset = function() {
-button1 = Math.floor((Math.random() * 12) + 1);
-$(".button1").attr("value",button1)
-button2 = Math.floor((Math.random() * 12) + 1);
-$(".button2").attr("value",button2)
-button3 = Math.floor((Math.random() * 12) + 1);
-$(".button3").attr("value",button3)
-button4 = Math.floor((Math.random() * 12) + 1);
-$(".button4").attr("value",button4)
-computerNum = Math.floor((Math.random() * 102) + 19);
-playerTotal = 0;
+//reset function, resets the randomizer, changes the value attr of buttons.
+var reset = function () {
+    computerNum = Math.floor((Math.random() * 102) + 19);
+    $(".computerNum").html(computerNum)
+    button1 = Math.floor((Math.random() * 12) + 1);
+    $(".button1").attr("value", button1)
+    button2 = Math.floor((Math.random() * 12) + 1);
+    $(".button2").attr("value", button2)
+    button3 = Math.floor((Math.random() * 12) + 1);
+    $(".button3").attr("value", button3)
+    button4 = Math.floor((Math.random() * 12) + 1);
+    $(".button4").attr("value", button4)
+    playerTotal = 0;
 }
 
 //add random button number to total score
-$("img").on("click", function() {
+$("img").on("click", function () {
     //"this" below refers to $("img").on above, NOT the window.
     var newTotal = playerTotal += parseInt($(this).attr("value"));
     $(".playerTotal").html(newTotal);
-   //player wins if total score matches the random goal number. wins ++
-    if (newTotal == computerNum) {
-        wins ++;
-        //game restarts regardless of outcome
-        //reset() for all numbers, no page refresh.
-        reset();
+   
+    //shout out to http://jsfiddle.net/UWYzr/ for showing me you can switch (true)
+    switch (true) {
+        case (newTotal === computerNum):
+            wins++;
+            //write wins to html's span for wins
+            $("#wins").html(wins)
+            //game restarts regardless of outcome
+            //reset() for all numbers, no page refresh.
+            reset();
+            break;
+        case (newTotal > computerNum):
+            losses++;
+            //write losses to html's span for losses.
+            $("#losses").html(losses)
+            //game restarts regardless of outcome
+            //reset() for all numbers, no page refresh.
+            reset()
+            break;
     }
-    //player busts if it goes over. losses ++
-    if (newTotal > computerNum) {
-        losses ++;
-        //game restarts regardless of outcome
-        //reset() for all numbers, no page refresh.
-        reset()
-    }
+});
 
-    })
-//display wins+losses directly on page with $("#id").html(var). 
+//This way also works but I opted to learn switch statements instead.
+//Stack overflow says the switch method is measurably slower.
+
+//  player wins if total score matches the random goal number. wins ++
+//     if (newTotal == computerNum) {
+//         wins++;
+//         //write wins to html's span for wins
+//         $("#wins").html(wins)
+//         //game restarts regardless of outcome
+//         //reset() for all numbers, no page refresh.
+//         reset();
+//     }
+//player busts if it goes over. losses ++
+//     else if (newTotal > computerNum) {
+//         losses++;
+//         //write losses to html's span for losses.
+//         $("#losses").html(losses)
+//         //game restarts regardless of outcome
+//         //reset() for all numbers, no page refresh.
+//         reset()
+//     }
+
+//     })
 
